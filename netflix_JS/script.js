18f04a253b2ff59f53b4ad57eee6182a19b007e1
visvanathan <128319329+Visva2000@users.noreply.github.com>
2023-07-25 20:35:57 +0530

    Update script.js

--- netflix_JS/script.js
+++ netflix_JS/script.js
@@ -1,11 +1,14 @@
 const button = document.querySelectorAll("[id='title']");
 const text = document.querySelectorAll("[id='text']");
+const symbol = document.querySelectorAll("[id='symbol']");
+
 
 for(let i=0;i<button.length;i++){
    button[i].addEventListener("click",()=>{
       text[i].classList.toggle("toggle")
       button[i].classList.toggle("buttontoggle")
     //   text[i].style.marginBottom ="10px";
+    symbol[i].classList.toggle("symbolRotate")
    
    })
 
